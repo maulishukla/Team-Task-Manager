0d89aa9594c94df3b9084432137b42f39ea792a6
Mauli Shukla <maulishukla19@gmail.com>
2026-05-02 02:35:20 +0530

    added project model

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
-  name: String,
+const ProjectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
   description: String,
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
-});
+  createdBy: String
+}, { timestamps: true });
 
-module.exports = mongoose.model("Project", projectSchema);
+module.exports = mongoose.model("Project", ProjectSchema);
