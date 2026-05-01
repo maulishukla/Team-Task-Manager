a88fc49a6ab005c6a6f1abebb5dd2424d5a3d3d1
Mauli Shukla <maulishukla19@gmail.com>
2026-05-02 02:42:05 +0530

    fix 502 binding issue

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("API Running");
+});
+
 // Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/projects", require("./routes/projects"));
